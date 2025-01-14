567b60ca8cb890429ac7d5496dc85b11db2bc9b9
thevowels <aungphyohtet1@gmail.com>
2025-01-14 09:15:54 +0700

    using custom theme on Card

--- src/components/Index/themes.tsx
+++ src/components/Index/themes.tsx
@@ -1,8 +1,6 @@
-import { Tabs, Tab, Card, CardBody } from "@nextui-org/react";
+import {Tabs, Tab, Card, CardBody, Image, RadioGroup, Radio} from "@nextui-org/react";
 import { useState, useMemo } from "react";
-
 import { shopCartStyles } from "./shopcartstyles";
-
 import {
   title,
   subtitle,
@@ -14,7 +12,9 @@ import {
   MagicIcon,
   GamingConsoleIcon,
   NextUILogo,
+    StarIcon
 } from "@/components/nextIcons";
+import {Button} from "@nextui-org/button";
 
 const themesTabs = (isMobile: boolean) => [
   {
@@ -63,6 +63,9 @@ const themesTabs = (isMobile: boolean) => [
   },
 ];
 
+
+const itemSizes = ["xs", "s", "m", "l", "xl"];
+
 // const codeHighlights = {
 //   nextui: "6-19",
 //   modern: "26-39",
@@ -93,121 +96,152 @@ const CustomThemesExample = ({
 
   const onSelectionChange = (value: React.Key) => {
     onChangeTheme(value as Theme);
+    console.log('changing selection', value)
   };
 
   return (
-      <div className="flex flex-col gap-6 ">
-
-        <Tabs
-            disableAnimation
-            disableCursorAnimation
-            aria-label="Dynamic tabs"
-            classNames={{
-              base: "w-full",
-              tab: "px-0 w-fit h-auto data-[selected=true]:bg-transparent",
-              tabList: "w-full justify-start gap-8",
-              tabContent: "text-default-400 text-base",
-            }}
-            items={tabs}
-            variant="light"
-        >
-          {(item) => (
-              <Tab
-                  key={item.id}
-                  title={
-                    <div className={"!bg-transparent"}>
-                      <div
-                          className={"flex flex-col justify-center items-center gap-2"}
-                      >
-                        {item.icon()}
-                        {item.title()}
-                      </div>
-                    </div>
-                  }
-              >
-                <Card>
-                  <CardBody>{item.id}</CardBody>
-                </Card>
-              </Tab>
-          )}
-        </Tabs>
-      </div>
-        );
-        };
-
-        export default function Themes() {
-        const isMobile: boolean = false;
-        const tabs = themesTabs(isMobile);
-
-        const [selectedTheme, setSelectedTheme] = useState<Theme>("nextui");
-
-        return (
-        <section className={sectionWrapper({class: "mt-24 lg:mt-56"})}>
-        <div className={"flex flex-col gap-8"}>
-          <div>
-            <div className={titleWrapper()}>
-              <p className={title({size: "lg"})}>Apply your own</p>
-              <div>
-                <p className={title({color: "blue", size: "lg"})}>theming </p>
-                <p className={title({size: "lg"})}>decisions.</p>
+    <div className="flex flex-col gap-6 ">
+      <Tabs
+        disableAnimation
+        disableCursorAnimation
+        aria-label="Dynamic tabs"
+        classNames={{
+          base: "w-full",
+          tab: "px-0 w-fit h-auto data-[selected=true]:bg-transparent",
+          tabList: "w-full justify-start gap-8",
+          tabContent: "text-default-400 text-base",
+        }}
+        items={tabs}
+        variant="light"
+        onSelectionChange={onSelectionChange}
+      >
+        {(item) => (
+          <Tab
+            key={item.id}
+            title={
+              <div className={"!bg-transparent"}>
+                <div
+                  className={"flex flex-col justify-center items-center gap-2"}
+                >
+                  {item.icon()}
+                  {item.title()}
+                </div>
               </div>
-            </div>
-            <p className={subtitle()}>
-              NextUI provides a custom TailwindCSS plugin that allows you to
-              customize the default themes or create your own.
-            </p>
+            }
+          />
+        )}
+      </Tabs>
+      <Card className={slots.wrapper()} radius={"lg"}>
+        <CardBody className="relative flex-col md:flex-row md:items-center gap-4 md:gap-9 overflow-visible">
+          <div className={slots.imageWrapper()}>
+            <Image
+                fill
+              removeWrapper
+              className={slots.img()}
+              sizes={"100vw"}
+              src="/shoes-1.png"
+              />
           </div>
+          <div className={slots.contentWrapper()}>
+            <div className={"relative flex flex-wrap items-baseline"}>
+              <h1 className={slots.title()}>Nike Adapt BB 2.0</h1>
+              <p className={slots.description()}>Consistent, customized fit, game-changing.</p>
+              <p className={slots.price()}>$279.97</p>
+              <p className={slots.previousPrice()}>$350</p>
+              <p className={slots.percentOff()}>20% off</p>
+            </div>
+            <RadioGroup
+              aria-label="select size"
+              classNames={{
+                base:"my-4",
+              }}
+              defaultValue={"xs"}
+              orientation="horizontal"
+              >
+              {itemSizes.map((itemSize) => (
+                  <Radio
+                    key={itemSize}
+                   classNames={{
+                     wrapper: "hidden",
+                     labelWrapper: slots.sizeOption(),
+                     label: slots.sizeOptionLabel(),
+                   }}
+                  value={itemSize}
+                  >
+                    {itemSize.toUpperCase()}
+                  </Radio>
+              ))}
+            </RadioGroup>
+            <div className={"flex space-x-4"}>
+              <Button
+                  className={slots.buyButton()}
+                  color="primary"
+                  variant={selectedTheme === "nextui" ? "shadow" : "solid"}
+              >
+                Buy now
+              </Button>
+              <Button
+                  className={slots.addToBagButton()}
+                  color="primary"
+                  radius="full"
+                  variant="bordered"
+              >
+                Add to bag
+              </Button>
+            </div>
+          </div>
+          <Button
+              isIconOnly
+              aria-label="like"
+              className={slots.starButton()}
+              data-liked={liked}
+              radius="full"
+              variant="light"
+              onPress={() => setLiked((v) => !v)}
+          >
+            <StarIcon fill={liked ? "currentColor" : "none"} size={20} />
+          </Button>
 
-          <div className={"grid grid-cols-1 lg:grid-cols-2 gap-4"}>
-            <CustomThemesExample
-                selectedTheme={"nextui"}
-                tabs={tabs}
-                onChangeTheme={setSelectedTheme}
-            />
+        </CardBody>
+      </Card>
+
+    </div>
+  );
+};
+
+export default function Themes() {
+  const isMobile: boolean = false;
+  const tabs = themesTabs(isMobile);
+
+  const [selectedTheme, setSelectedTheme] = useState<Theme>("nextui");
+
+  return (
+    <section className={sectionWrapper({ class: "mt-24 lg:mt-56" })}>
+      <div className={"flex flex-col gap-8"}>
+        <div>
+          <div className={titleWrapper()}>
+            <p className={title({ size: "lg" })}>Apply your own</p>
             <div>
-
-              asdfasddfk
-              asdfas
-              dfa
-              sdf
+              <p className={title({ color: "blue", size: "lg" })}>theming </p>
+              <p className={title({ size: "lg" })}>decisions.</p>
             </div>
-            {/*<Tabs*/}
-            {/*  disableAnimation*/}
-            {/*  disableCursorAnimation*/}
-            {/*  aria-label="Dynamic tabs"*/}
-            {/*  classNames={{*/}
-            {/*    base: "w-full",*/}
-            {/*    tab: "px-0 w-fit h-auto data-[selected=true]:bg-transparent",*/}
-            {/*    tabList: "w-full justify-start gap-8",*/}
-            {/*    tabContent: "text-default-400 text-base",*/}
-            {/*  }}*/}
-            {/*  items={tabs}*/}
-            {/*  variant="light"*/}
-            {/*>*/}
-            {/*  {(item) => (*/}
-            {/*    <Tab*/}
-            {/*      key={item.id}*/}
-            {/*      title={*/}
-            {/*        <div className={"!bg-transparent"}>*/}
-            {/*          <div*/}
-            {/*            className={*/}
-            {/*              "flex flex-col justify-center items-center gap-2"*/}
-            {/*            }*/}
-            {/*          >*/}
-            {/*            {item.icon()}*/}
-            {/*            {item.title()}*/}
-            {/*          </div>*/}
-            {/*        </div>*/}
-            {/*      }*/}
-            {/*    >*/}
-            {/*      <Card>*/}
-            {/*        <CardBody>{item.id}</CardBody>*/}
-            {/*      </Card>*/}
-            {/*    </Tab>*/}
-            {/*  )}*/}
-            {/*</Tabs>*/}
           </div>
+          <p className={subtitle()}>
+            NextUI provides a custom TailwindCSS plugin that allows you to
+            customize the default themes or create your own.
+          </p>
         </div>
-      </section>
+
+        <div className={"grid grid-cols-1 lg:grid-cols-2 gap-4"}>
+          <CustomThemesExample
+            selectedTheme={selectedTheme}
+            tabs={tabs}
+            onChangeTheme={setSelectedTheme}
+          />
+          <div>asdfasddfk asdfas dfa sdf</div>
+
+        </div>
+      </div>
+    </section>
   );
 }
